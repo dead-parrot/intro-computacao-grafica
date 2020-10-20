e41a8c0dbf7149deb08e6a61759c862ecdaf23e5
Rogério Marinho <46401377+dead-parrot@users.noreply.github.com>
2020-10-19 22:02:40 -0300

    att

--- Trabalho-2/mygl.c
+++ Trabalho-2/mygl.c
@@ -29,19 +29,20 @@ void DrawLine(Ponto inicial, Ponto final, Cor cor1, Cor cor2)
 {
     int dx = final.x - inicial.x;
     int dy = final.y - inicial.y;
-    int incremento_x = 1;
-    int incremento_y;
+    int incremento_x = 1; //Variável para incremento no eixo do x 
+    int incremento_y;     //Variável para incremento no eixo do y. Varia se y cresce ou decresce
 
 
-    int absdx = dx;
-    int absdy = dy;
+    int absdx = dx; //Valor absoluto de dx
+    int absdy = dy; //Valor absoluto de dy
 
-    if (absdy < 0)
+    /*Caso os valores anteriores sejam negativos*/
+    if (absdy < 0) 
         absdy = - dy;
     if (absdx < 0)
         absdx = -dx;
 
-    int c;
+    int c; //tamanho do maior eixo
     if (absdx >= absdy)
         c = absdx;
     else 
@@ -52,17 +53,17 @@ void DrawLine(Ponto inicial, Ponto final, Cor cor1, Cor cor2)
     float acrescimosBlue = (cor2.blue - cor1.blue)/c;
     float acrescimosAlpha = (cor2.alpha - cor1.alpha)/c; 
 
-    if (dx == 0 || dy == 0)
+    if (dx == 0 || dy == 0) //Caso as linhas sejam horizontais ou verticais
     {
-        int plus;
+        int plus; 
         Ponto pontoAuxiliar = inicial;
 
-        if(dy == 0){
+        if(dy == 0){ //Reta horizontal
             if (dx < 0)
-                plus = -1;
+                plus = -1; //Da esquerda pra direita
             else
             {
-                plus = 1;
+                plus = 1; //Da direita pra esquerda
             }
 
             while(pontoAuxiliar.x != final.x)
@@ -75,11 +76,11 @@ void DrawLine(Ponto inicial, Ponto final, Cor cor1, Cor cor2)
                 cor1.alpha+=acrescimosAlpha;
             }
         }
-        else
+        else //Reta vertical
         {
-            if (dy < 0)
+            if (dy < 0) //De cima pra baixo
                 plus = -1;
-            else
+            else //De baixo pra cima
                 plus = 1;
             
             while(pontoAuxiliar.y != final.y)
@@ -96,18 +97,21 @@ void DrawLine(Ponto inicial, Ponto final, Cor cor1, Cor cor2)
         return;
     }
 
-    if (dx < 0)// Para os quadrantes 3 a 6
+
+    /*Para as linhas do quadrantes 3 a 6, desenhamos ao contrário para virar uma linha do quadrante oposto*/
+    if (dx < 0)
     {
         DrawLine(final, inicial, cor2, cor1);
         return;
     }
 
+    //se y está decrescendo, ao criar a linhas vamos diminuindo o valor de y do próximo pixel 
     if (dy < 0)
     {
         incremento_y = -1;
         dy = -dy;
     }
-    else
+    else//caso contrário, aumentaremos
     {
         incremento_y = 1;
     }
@@ -131,14 +135,14 @@ void DrawLine(Ponto inicial, Ponto final, Cor cor1, Cor cor2)
             D += incremento_NE;
             (aux.y) += incremento_y;
             (aux.x) += incremento_x;
-        }
+        } 
         else
         {
             
             D += incremento_L;
-            if (absdx >= absdy)
+            if (absdx >= absdy) //se x for o maior eixo, andaremos(incrementaremos) só no x
                 (aux.x) += incremento_x;
-            else
+            else//caso contrário, andaremos(incrementaremos) só no y
             {
                 (aux.y) += incremento_y;
             }
@@ -148,6 +152,7 @@ void DrawLine(Ponto inicial, Ponto final, Cor cor1, Cor cor2)
 }
 
 
+/*Para desenhar um triangulo, ligamos os vértices */
 void drawTriangle(Ponto ponto1, Ponto ponto2, Ponto ponto3, Cor cor1, Cor cor2, Cor cor3)
 {
     DrawLine(ponto1, ponto2, cor1, cor2);
@@ -158,7 +163,7 @@ void drawTriangle(Ponto ponto1, Ponto ponto2, Ponto ponto3, Cor cor1, Cor cor2, 
 // Definição da função que chamará as funções implementadas pelo aluno
 void MyGlDraw(void) {
     //
-    // >>> Chame aqui as funções que você implementou <<<
+    // >>> Exemplos de testes <<<
     //
     Cor vermelho;
     vermelho.red = 255;
@@ -226,6 +231,10 @@ void MyGlDraw(void) {
     a.y = 0;
     b.x = 256;
     b.y = 511;
-    DrawLine(a, b, azul, verde);       
+    DrawLine(a, b, azul, verde);
+
+    a.x = 350;
+    a.y = 300; 
+    PutPixel(a, vermelho);      
 
 }
